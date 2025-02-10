76888314f88badafd6696c2efcda9c465a60cd23
avitel99 <157854609+avitel99@users.noreply.github.com>
2025-02-10 08:23:03 -0500

    Update version1.py

--- cryptography-directory/version1.py
+++ cryptography-directory/version1.py
@@ -1,7 +1,8 @@
+#uses a1.txt file
 import random
 
-u=list(input())
-num=int(input())
+u=list(input('enter text'))
+num=int(input('key'))
 
 a=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r','s', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 cap=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
